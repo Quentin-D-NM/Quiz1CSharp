5c942fb34e990d87e96730376a901e3682593f28
Quentin-D-NM <qdye2001@gmail.com>
2021-07-06 22:37:22 -0600

    Initial Commit

---
+++ Quiz1/ForceCalc.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Quiz1
+{
+    class ForceCalc
+    {
+        public const double G = 6.67408e-11;
+        private double object1;
+        private double object2; 
+        private double distance;
+        private double gravitationalForce;
+
+        public double Object1 { get { return object1; } set { object1 = value; } }
+        public double Object2 { get { return object2; } set { object2 = value;  } }
+
+        public double Distance { get { return distance; } set { distance = value; Calc(); } }
+
+        public double GravitationalForace { get { return gravitationalForce; } private set { gravitationalForce = value; } }
+
+        private void Calc()
+        {
+            GravitationalForace = G * ((Object1 * Object2) / Math.Pow(Distance, 2));
+        }
+
+    }
+}
---
+++ Quiz1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Quiz1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string answer = "n";
+
+            do
+            {
+                ForceCalc fc = new ForceCalc();
+
+                Console.WriteLine("Please enter the mass of your first object in kilograms: ");
+                fc.Object1 = Double.Parse(Console.ReadLine());
+                Console.WriteLine("Please enter the mass of your second object in kilograms: ");
+                fc.Object2 = Double.Parse(Console.ReadLine());
+                Console.WriteLine("Please enter the distance between the two objects in meters: ");
+                fc.Distance = Double.Parse(Console.ReadLine());
+
+                Console.WriteLine("The gravitational force between the two objects is: {0}", fc.GravitationalForace);
+
+
+
+                Console.WriteLine("Would you like to make another Calculation?(Y or n) ");
+                answer = Console.ReadLine();
+            } while (answer == "y" || answer == "Y");
+        }
+    }
+}
